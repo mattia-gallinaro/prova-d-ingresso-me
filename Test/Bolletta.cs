c73d7adb106ca963c68c4a23d67fc023f3793ef3
mattia-gallinaro <gallinaromattia6@gmail.com>
2022-09-27 12:17:55 +0200

    .

--- Test/Bolletta.cs
+++ Test/Bolletta.cs
@@ -9,21 +9,21 @@ namespace Test
         //variabili che indicano i costi fissi per le bollette del gas e della luce
         protected internal double quota_fissa, spesa_tp_gc;
         //costi all'unitá sia per kwh che per smc
-        protected internal double costo_per_unita, costo_tp_gc_unita, oneri, quota_vendita_unitá;
+        protected internal double costo_per_unita, costo_tp_gc_unita, oneri, quota_vendita_unita;
 
-        public Bolletta(double costo_per_unita, double quota_fissa, double spesa_tp_gc, double costo_tp_gc_unita, double oneri, double quota_vendita_unitá)//assegna i valori alle variabili omonime
+        public Bolletta(double costo_per_unita, double quota_fissa, double spesa_tp_gc, double costo_tp_gc_unita, double oneri, double quota_vendita_unita)//assegna i valori alle variabili omonime
         {
             this.costo_per_unita = costo_per_unita;
             this.quota_fissa = quota_fissa;
             this.spesa_tp_gc = spesa_tp_gc;
             this.costo_tp_gc_unita = costo_tp_gc_unita;
             this.oneri = oneri;
-            this.quota_vendita_unitá = quota_vendita_unitá;
+            this.quota_vendita_unita = quota_vendita_unita;
         }
 
         public double Costo_tot_bolletta(double consumo)//calcola il prezzo in base al consumo della macchina e il valore delle variabili
         {
-            double costo = costo_per_unita * consumo + quota_vendita_unitá * consumo + quota_fissa * 12 + (spesa_tp_gc + costo_tp_gc_unita * consumo) + oneri * consumo;//calcola quanto costa la bolletta annualmente in base al consumo del macchinario
+            double costo = costo_per_unita * consumo + quota_vendita_unita * consumo + quota_fissa * 12 + (spesa_tp_gc + costo_tp_gc_unita * consumo) + oneri * consumo;//calcola quanto costa la bolletta annualmente in base al consumo del macchinario
             return costo;
         }
     }
